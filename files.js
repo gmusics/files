45d9360629a8f244289e5eaecc04d2d3ae1d9481
Gmusics <gmusics.io@gmail.com>
2020-11-18 14:04:54 +0100

    update

---
+++ files.js
@@ -0,0 +1,16 @@
+  [{
+    id:1,
+    artist: 'first artist',
+    title: 'fisrt artist song',
+    song_url: 'my_song.mp3',
+    img: 'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_1280.png'
+  },
+    {
+      id: 2,
+      artist: 'second artist',
+      title: 'Way To Fly',
+      song_url: 'my_song.mp3',
+      img: 'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_1280.png'
+    },
+  ];
+  
